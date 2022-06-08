c427b94ba4091cbcbd70a9281ecae6d65068195b
ulises-david <ulisesdavidacosta27@gmail.com>
2022-06-08 16:18:20 -0300

    user hash password done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 require("./api/database/data")
+const morgan = require('morgan')
 
 const userRoutes = require('./api/routes/user.routes')
 
@@ -8,8 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(morgan('dev'));
 
-app.use(userRoutes)
+app.use("/users", userRoutes)
 
 app.listen(process.env.PORT);
 console.log("Server is running");
