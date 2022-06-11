60d9ba36f81f1ea11d0c9ce4b2334d64603a1353
ulises-david <ulisesdavidacosta27@gmail.com>
2022-06-11 19:39:53 -0300

    missing auth

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
-const { obtainUser, obtainOneUser } = require('../controllers/user.controllers')
+const { obtainUser, obtainOneUser, createNewUser } = require('../controllers/user.controllers')
 const router = Router()
 
 // router.get('/', sayHi)
 router.get('/', obtainUser)
 router.get('/:id', obtainOneUser)
+router.get('/', createNewUser)
+
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 const express = require("express");
-const passport = require("passport");
 const morgan = require("morgan");
 const cors = require("cors");
-const session = require("express-session");
 require("dotenv").config();
-require("./api/auth/passport");
 require("./api/database/data");
 require("./api/asociations/asociations");
 
 const userRoutes = require("./api/routes/user.routes");
 const budgetsRoutes = require("./api/routes/budget.routes");
-const authUser = require("./api/routes/loginUser.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(
-  session({
-    secret: process.env.SECRETSESSION,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 app.use(morgan("dev"));
-
-app.use(passport.authenticate());
-app.use(passport.session());
 
 app.use("/users", userRoutes);
 app.use("/budgets", budgetsRoutes);
-app.use("/auth", authUser);
 
 app.listen(process.env.PORT);
 console.log("Server is running");
